0342a602dd2a018f8413b0a27fcc1ccf3ef10c08
Jonathan Moore <jmjonemoore@gmail.com>
2019-08-02 10:29:17 -0500

    On user story 1

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Route } from 'react-router-dom'
+import LandingPage from './routes/LandingPage/LandingPage'
+import './App.css';
+
+export default class App extends React.Component {
+  mainRoutes(){
+    return(
+      <>
+      <Route
+      exact
+      key='/'
+      path='/'
+      component={LandingPage}
+      />
+      </>
+    )
+  }
+  
+  render(){
+  return (
+    <div className="App">
+      <main className = 'App_main'>
+        {this.mainRoutes()}
+      </main>
+    </div>
+  );
+  }
+}
+
+
---
+++ src/routes/LandingPage/LandingPage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default class LandingPage extends React.Component {
+  render(){
+    return(
+      <div>
+        <header>
+          <h1>
+            Welcome to Petful
+          </h1>
+        </header>
+        <p>
+          To start the adoption process, click the start button below. 
+          You'll be shown a cat and a dog that are available for adoption.
+          You'll get a name of the pet, their gender, age, breed, and their story.
+          If you're first in line, you will the option to adopt either pet. 
+        </p>
+        <button type="button">Start</button>
+
+      </div>
+    )
+  }
+}
