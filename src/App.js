0c1f006023e3e775067dcc2d3197c5e143c8808d
Jonathan Moore <jmjonemoore@gmail.com>
2019-08-02 10:39:29 -0500

    Created adopt page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import { Route } from 'react-router-dom'
 import LandingPage from './routes/LandingPage/LandingPage'
+import AdoptPage from './routes/AdoptPage/AdoptPage'
 import './App.css';
 
 export default class App extends React.Component {
@@ -13,6 +14,13 @@ export default class App extends React.Component {
       key='/'
       path='/'
       component={LandingPage}
+      />
+
+      <Route
+      exact
+      key='/adopt'
+      path='/adopt'
+      component={AdoptPage}
       />
       </>
     )
