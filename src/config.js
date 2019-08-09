c81bef07969c4c4b0b7f94549bb60a8bb0308719
Dustin Haefele <Dustin.haefele@gmail.com>
2019-08-09 14:07:44 -0500

    changed back to local host for development

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://intense-lowlands-42230.herokuapp.com/api'
+    API_ENDPOINT: 'http://localhost:8080/api'
+  // API_ENDPOINT: 'https://intense-lowlands-42230.herokuapp.com/api'
 }
